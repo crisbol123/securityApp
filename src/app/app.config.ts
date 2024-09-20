a64573a62d123e4628c4a9323a5d1630b847bc31
cristianfbolanos@unicauca.edu.co <cristianfbolanosfelip@LAPTOP-D4DRQ08Q MINGW64 ~/aplicacion (master)$ git initReinitialized existing Git repository in C:/Users/felip/aplicacion/.git/felip@LAPTOP-D4DRQ08Q MINGW64 ~/aplicacion (master)$ git add.git: 'add.' is not a git command. See 'git --help'.The most similar command is        addfelip@LAPTOP-D4DRQ08Q MINGW64 ~/aplicacion (master)$ git add .felip@LAPTOP-D4DRQ08Q MINGW64 ~/aplicacion (master)$felip@LAPTOP-D4DRQ08Q MINGW64 ~/aplicacion (master)$ git commit -m primer commiterror: pathspec 'commit' did not match any file(s) known to gitfelip@LAPTOP-D4DRQ08Q MINGW64 ~/aplicacion (master)$ git commit -m primer>
2024-09-19 22:14:18 -0500

    componente login

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,25 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideClientHydration } from '@angular/platform-browser';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { importProvidersFrom } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideRouter(routes),
+    provideClientHydration(),
+    provideAnimationsAsync(),
+    importProvidersFrom(
+      MatCardModule,
+      MatFormFieldModule,
+      MatInputModule,
+      MatButtonModule
+    ), provideAnimationsAsync()
+  ]
 };
