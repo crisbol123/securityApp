3c484323fe092ca485ba21e3731d7c6ea32b5d6a
cristianfbolanos@unicauca.edu.co <cristianfbolanosfelip@LAPTOP-D4DRQ08Q MINGW64 ~/aplicacion (master)$ git initReinitialized existing Git repository in C:/Users/felip/aplicacion/.git/felip@LAPTOP-D4DRQ08Q MINGW64 ~/aplicacion (master)$ git add.git: 'add.' is not a git command. See 'git --help'.The most similar command is        addfelip@LAPTOP-D4DRQ08Q MINGW64 ~/aplicacion (master)$ git add .felip@LAPTOP-D4DRQ08Q MINGW64 ~/aplicacion (master)$felip@LAPTOP-D4DRQ08Q MINGW64 ~/aplicacion (master)$ git commit -m primer commiterror: pathspec 'commit' did not match any file(s) known to gitfelip@LAPTOP-D4DRQ08Q MINGW64 ~/aplicacion (master)$ git commit -m primer>
2024-09-20 08:06:19 -0500

    se creo componente

--- src/app/registro-datos/registro-datos.component.spec.ts
+++ src/app/registro-datos/registro-datos.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { LoginComponent } from './login.component';
+import { RegistroDatosComponent } from './registro-datos.component';
 
-describe('LoginComponent', () => {
-  let component: LoginComponent;
-  let fixture: ComponentFixture<LoginComponent>;
+describe('RegistroDatosComponent', () => {
+  let component: RegistroDatosComponent;
+  let fixture: ComponentFixture<RegistroDatosComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [LoginComponent]
+      imports: [RegistroDatosComponent]
     })
     .compileComponents();
     
-    fixture = TestBed.createComponent(LoginComponent);
+    fixture = TestBed.createComponent(RegistroDatosComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
---
+++ src/app/registro-datos/registro-datos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registro-datos',
+  standalone: true,
+  imports: [],
+  templateUrl: './registro-datos.component.html',
+  styleUrl: './registro-datos.component.css'
+})
+export class RegistroDatosComponent {
+
+}
