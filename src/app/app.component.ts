eacad856a161db154ab7df194d13b6d2921a8789
cristianfbolanos@unicauca.edu.co <cristianfbolanosfelip@LAPTOP-D4DRQ08Q MINGW64 ~/aplicacion (master)$ git initReinitialized existing Git repository in C:/Users/felip/aplicacion/.git/felip@LAPTOP-D4DRQ08Q MINGW64 ~/aplicacion (master)$ git add.git: 'add.' is not a git command. See 'git --help'.The most similar command is        addfelip@LAPTOP-D4DRQ08Q MINGW64 ~/aplicacion (master)$ git add .felip@LAPTOP-D4DRQ08Q MINGW64 ~/aplicacion (master)$felip@LAPTOP-D4DRQ08Q MINGW64 ~/aplicacion (master)$ git commit -m primer commiterror: pathspec 'commit' did not match any file(s) known to gitfelip@LAPTOP-D4DRQ08Q MINGW64 ~/aplicacion (master)$ git commit -m primer>
2024-09-20 10:28:57 -0500

    cambios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,12 +5,14 @@ import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { RegistroDatosComponent } from "./registro-datos/registro-datos.component";
+import { LoadingSpinnerComponent } from "./loading-spinner/loading-spinner.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginComponent, MatCardModule, MatFormFieldModule, MatInputModule, MatButtonModule],
+  imports: [RouterOutlet, LoginComponent, MatCardModule, MatFormFieldModule, MatInputModule, MatButtonModule, RegistroDatosComponent, LoadingSpinnerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/registro-datos/registro-datos.component.ts
+++ src/app/registro-datos/registro-datos.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { LoadingSpinnerComponent } from "../loading-spinner/loading-spinner.component";
 
 @Component({
   selector: 'app-registro-datos',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule, MatButtonModule, MatInputModule, MatCardModule, MatFormFieldModule, MatSelectModule, LoadingSpinnerComponent],
   templateUrl: './registro-datos.component.html',
-  styleUrl: './registro-datos.component.css'
+  styleUrls: ['./registro-datos.component.css']
 })
-export class RegistroDatosComponent {
+export class RegistroDatosComponent implements OnInit {
+  registroForm!: FormGroup;
+  huellaRegistrada: boolean = false;
+  tomandoHuella: boolean = false;
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.registroForm = this.fb.group({
+      cedula: ['', [Validators.required, Validators.pattern('^[0-9]*$')]],
+      nombre: ['', Validators.required],
+      tipoUsuario: ['', Validators.required]
+    });
+  }
+
+  onRegistrarHuella(): void {
+    this.tomandoHuella = true;
+    setTimeout(() => {
+      this.tomandoHuella = false;
+      this.huellaRegistrada = true;
+    }, 3000); // Simula la toma de huella durante 3 segundos
+  }
+
+  hasErrors(controlName: string, errorName: string): boolean {
+    return this.registroForm.controls[controlName].hasError(errorName);
+  }
+  
 }
