281a46d2abf76f18da244c6ab8062dadde35b55e
JDaniloN <danilonoguera29@gmail.com>
2024-10-12 19:41:22 -0500

    Final chat

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -12,7 +12,9 @@ import { CommonModule } from '@angular/common';
 })
 export class ChatComponent implements OnInit {
   currentStep: Step | null = null;  // Inicializamos como null para evitar el error
+  stepHistory: Step [] = [];
   conversationHistory: { sender: string; message: string }[] = [];
+  isMinimized = false;
 
   constructor(private chatService: ChatService) {}
 
@@ -22,21 +24,46 @@ export class ChatComponent implements OnInit {
   }
 
   selectOption(option: Option) {
+    if (this.currentStep) {
+      this.stepHistory.push(this.currentStep)
+    }
+
     this.addToHistory('user', option.text);
+    if (option.response){
+      this.addToHistory('bot',option.response)
+    }
+
+    if(option.next){
+      this.currentStep = option.next;
+      this.addToHistory('bot', this.currentStep.message);
+    }else{
+      this.currentStep = null;
+    }
     this.chatService.selectOption(option);
     this.currentStep = this.chatService.getCurrentStep();
-    this.addToHistory('bot', this.currentStep.message);
+  }
+
+  goBack() {
+    // Volver al paso anterior si existe
+    if (this.stepHistory.length) {
+      this.currentStep = this.stepHistory.pop() as Step;
+     }
   }
 
   resetChat() {
     this.chatService.resetConversation();
     this.currentStep = this.chatService.getCurrentStep();
+    this.stepHistory = [];
     this.conversationHistory = [];
     this.addToHistory('bot', this.currentStep.message);
   }
 
   private addToHistory(sender: string, message: string) {
     this.conversationHistory.push({ sender, message });
+  }
+
+  toggleChat(){
+    this.isMinimized = !this.isMinimized;
   }
 }
 
