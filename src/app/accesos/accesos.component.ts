88740744992d34f9377e5d7358f4c818dd6ca22d
Miguel Ortiz <80050612+LuisMiguel-efe@users.noreply.github.com>
2024-11-14 19:34:40 -0500

    Avance en graficas estadisticas
    Se añadio filtros por mes a todas las graficas y los kpis

--- src/app/accesos/accesos.component.ts
+++ src/app/accesos/accesos.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Acceso, AccesosService } from '../accesos.service';
 import { HttpClientModule } from '@angular/common/http';
@@ -23,11 +23,13 @@ Chart.register(...registerables);
 })
 
 export class AccesosComponent implements OnInit {
-  accesos: Acceso[] = [];
-  selectedPorteria: number | null = null;
-  selectedMonth: string = ''; // Almacena el mes seleccionado en formato abreviado
-  private myChart: Chart | null = null;
-  private myChart2: Chart<'pie', number[], string> | null = null;
+
+  accesos: Acceso[] = []; // Lista de accesos recopilados de la base de datos
+  selectedPorteria: number | null = null; // Porteria seleccionada en la interfaz
+  selectedMonth: string = ''; // Almacena el mes seleccionado en la interfaz de las graficas
+  private myChart: Chart | null = null; // Almacena las graficas de lineas, barras e histogramas
+  private myChart2: Chart<'pie', number[], string> | null = null; // Almacena las graficas circulares de procentajes
+  graficoSeleccionado: string = 'first'; // Variable para obtener el tipo de gráfico seleccionado y graficarlo
 
   // Variables para KPIs
   totalAccesos: number = 0;
@@ -35,8 +37,13 @@ export class AccesosComponent implements OnInit {
   diaMayorAcceso: string = '';
   porteriaFrecuente: number | null = null;
 
-  // Variable para gestionar el tipo de gráfico seleccionado
-  graficoSeleccionado: string = 'first';
+  // Referencias a cada boton de graficas
+  @ViewChild('accesosBtn') accesosBtn!: ElementRef;
+  @ViewChild('porteriasBtn') porteriasBtn!: ElementRef;
+  @ViewChild('tendenciasBtn') tendenciasBtn!: ElementRef;
+  @ViewChild('porcentajeBtn') porcentajeBtn!: ElementRef;
+  @ViewChild('histogramaBtn') histogramaBtn!: ElementRef;
+
 
   constructor(private accesosService: AccesosService) {}
 
@@ -49,38 +56,26 @@ export class AccesosComponent implements OnInit {
     this.selectedPorteria = porteria;
     this.obtenerAccesos(porteria);
   }
-
-  // Obtener accesos por portería desde el servicio
+  // Obtener accesos por portería desde el servicio backend
   obtenerAccesos(porteria: number): void {
-    this.accesosService.obtenerAccesosPorPorteria(porteria).subscribe((data) => {
-      this.accesos = data;
-    });
-  }
+    // Si la opcion seleccionado es "general" la porteria "0" recolectaria todos los accesos de todas las porterias
+    if(porteria==0){
+      // Obtener todos los accesos y Activar el boton para ver estadisticas de porterias más utilizadas
+      this.accesosService.getAllAccesos().subscribe((data) => {
+        this.accesos = data;
+        this.porteriasBtn.nativeElement.disabled = false;
+      });
+    } else{
+      // Obtener accesos de una determinada porteria y Desactivar el boton para ver estadisticas de porterias más utilizadas
+      this.accesosService.obtenerAccesosPorPorteria(porteria).subscribe((data) => {
+        this.accesos = data;
+        this.porteriasBtn.nativeElement.disabled = true;
+      });
+    }
 
-  // Extraer día de la fecha
-  extraerDia(fecha: string): string {
-    return fecha.split(' ')[2]; // Día es el tercer elemento
   }
-
-  // Extraer mes de la fecha (abreviado)
-  extraerMes(fecha: string): string {
-    return fecha.split(' ')[1]; // Mes es el segundo elemento
-  }
-
-  // Extraer hora de la fecha
-  extraerHora(fecha: string): string {
-    return fecha.split(' ')[3]; // Hora es el cuarto elemento
-  }
-
-  // Cambiar el tipo de gráfico seleccionado y generar reporte
-  obtenerTipo(tipo: string): void {
-    this.graficoSeleccionado = tipo;
-    this.generarReportes();
-  }
-
   // Generar reportes según el gráfico seleccionado
   generarReportes(): void {
-
     if (this.myChart) {
       this.myChart.destroy();
     }
@@ -112,7 +107,6 @@ export class AccesosComponent implements OnInit {
             block: "center"
           });
         }
-
         break;
       case 'histogram':
         // Mostrar el canvas principal y ocultar el circular
@@ -136,26 +130,19 @@ export class AccesosComponent implements OnInit {
 
 
   }
-
-  // Actualizar el gráfico cuando cambie el mes seleccionado
-  onMonthChange(): void {
-    this.generarReportes();
-  }
-
   // Calcular los KPIs (indicadores clave de rendimiento)
   calcularKPIs(): void {
     this.totalAccesos = this.accesos.length;
     const autenticados = this.accesos.filter(acceso => acceso.autenticado).length;
+    const autenticos = this.accesos.filter(acceso => acceso.autenticado);
     this.porcentajeAutenticados = (autenticados / this.totalAccesos) * 100;
-
     // Cálculo del día con más accesos
-    const accesosPorDia = this.accesos.reduce((acc, acceso) => {
+    const accesosPorDia = autenticos.reduce((acc, acceso) => {
       const dia = this.extraerDia(acceso.fecha_hora);
       acc[dia] = (acc[dia] || 0) + 1;
       return acc;
     }, {} as Record<string, number>);
     this.diaMayorAcceso = Object.keys(accesosPorDia).reduce((a, b) => accesosPorDia[a] > accesosPorDia[b] ? a : b);
-
     // Cálculo de la portería más utilizada
     const accesosPorPorteria = this.accesos.reduce((acc, acceso) => {
       acc[acceso.porteria] = (acc[acceso.porteria] || 0) + 1;
@@ -164,6 +151,32 @@ export class AccesosComponent implements OnInit {
     this.porteriaFrecuente = +Object.keys(accesosPorPorteria).reduce((a, b) =>
       accesosPorPorteria[+a] > accesosPorPorteria[+b] ? a : b
     );
+
+    if(this.selectedMonth){
+      const accesosFiltrados = this.accesos.filter(acceso => {
+        return this.selectedMonth ? this.extraerMes(acceso.fecha_hora) === this.selectedMonth : true;
+      });
+
+      this.totalAccesos = accesosFiltrados.length;
+      const autenticados = accesosFiltrados.filter(acceso => acceso.autenticado).length;
+      const autenticos = accesosFiltrados.filter(acceso => acceso.autenticado);
+      this.porcentajeAutenticados = (autenticados / this.totalAccesos) * 100;
+      // Cálculo del día con más accesos
+      const accesosPorDia = autenticos.reduce((acc, acceso) => {
+        const dia = this.extraerDia(acceso.fecha_hora);
+        acc[dia] = (acc[dia] || 0) + 1;
+        return acc;
+      }, {} as Record<string, number>);
+      this.diaMayorAcceso = Object.keys(accesosPorDia).reduce((a, b) => accesosPorDia[a] > accesosPorDia[b] ? a : b);
+      // Cálculo de la portería más utilizada
+      const accesosPorPorteria = accesosFiltrados.reduce((acc, acceso) => {
+        acc[acceso.porteria] = (acc[acceso.porteria] || 0) + 1;
+        return acc;
+      }, {} as Record<number, number>);
+      this.porteriaFrecuente = +Object.keys(accesosPorPorteria).reduce((a, b) =>
+        accesosPorPorteria[+a] > accesosPorPorteria[+b] ? a : b
+      );
+    }
   }
 
   // Gráfico de accesos mensuales filtrado por el mes seleccionado
@@ -196,9 +209,16 @@ export class AccesosComponent implements OnInit {
 
   // Gráfico circular para autenticados vs no autenticados
   private generarGraficoCircular(): void {
-    const autenticados = this.accesos.filter(acceso => acceso.autenticado).length;
-    const noAutenticados = this.accesos.length - autenticados;
+    var autenticados = this.accesos.filter(acceso => acceso.autenticado).length;
+    var noAutenticados = this.accesos.length - autenticados;
     const ctx = document.getElementById('myChart2') as HTMLCanvasElement;
+    if(this.selectedMonth){
+      const accesosFiltrados = this.accesos.filter(acceso => {
+        return this.selectedMonth ? this.extraerMes(acceso.fecha_hora) === this.selectedMonth : true;
+      });
+      autenticados = accesosFiltrados.filter(acceso => acceso.autenticado).length;
+      noAutenticados = accesosFiltrados.length - autenticados;
+    }
     // Crear el gráfico circular con tamaño reducido
     this.myChart2 = new Chart(ctx, {
       type: 'pie',
@@ -255,7 +275,6 @@ export class AccesosComponent implements OnInit {
     });
   }
 
-  // Histograma de accesos por hora
 // Histograma de accesos por hora (acumulado por hora completa)
 private generarHistogramaHoras(): void {
   const etiquetasHoras = this.generarEtiquetasHoras();
@@ -296,20 +315,31 @@ private generarHistogramaHoras(): void {
     }
     return horas;
   }
-  public getAllAccesos(): void {
-    this.accesosService.getAllAccesos().subscribe((data) => {
-      this.selectedPorteria = 3;
-      this.accesos = data;
-    });
+
+
+  // Funciones utilizadas
+  // Extraer informacion de las fechas en formato manejable
+  // Extraer día de la fecha
+  extraerDia(fecha: string): string {
+    return fecha.split(' ')[2]; // Día es el tercer elemento
+  }
+  // Extraer mes de la fecha (abreviado)
+  extraerMes(fecha: string): string {
+    return fecha.split(' ')[1]; // Mes es el segundo elemento
+  }
+  // Extraer hora de la fecha
+  extraerHora(fecha: string): string {
+    return fecha.split(' ')[3]; // Hora es el cuarto elemento
+  }
+  // Segun el gráfico seleccionado, generar reportes y mostrar graficas
+  // Cambiar el tipo de gráfico seleccionado y generar reporte
+  obtenerTipo(tipo: string): void {
+    this.graficoSeleccionado = tipo;
+    this.generarReportes();
+  }
+  // Actualizar el gráfico cuando cambie el mes seleccionado
+  onMonthChange(): void {
+    this.generarReportes();
   }
 
 }
-
-
-
-
-
-
-
-
-
